0e3fceb38c8b01f003339233df0c082fcb1450d2
faustis1337 <faustis1337@gmail.com>
2023-04-16 19:42:35 +0200

    cors

--- SearchAPI/Program.cs
+++ SearchAPI/Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
